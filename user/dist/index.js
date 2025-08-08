c2064c66ff66d0da9f454be27717a87927f3f971
vikas1545 <vikaskumar.vk0612@gmail.com>
2025-08-08 12:15:49 +0530

    project initilized

---
+++ user/dist/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { createClient } from "redis";
+dotenv.config();
+connectDb();
+export const redisClient = createClient({ url: process.env.REDIS_URL });
+redisClient
+    .connect()
+    .then(() => console.log("Connected to Redis"))
+    .catch((error) => {
+    console.error("Error in Redis connection:", error);
+});
+const app = express();
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`server started on ${port}`);
+});
---
+++ user/src/model/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+}
+
+const schema: Schema<IUser> = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model<IUser>("User", schema);
