592d7625ea84c3b8a31daae41674b522c1b178e9
vikas1545 <vikaskumar.vk0612@gmail.com>
2025-08-12 10:40:52 +0530

    all services initialized

--- user/dist/config/rabbitmq.js
+++ user/dist/config/rabbitmq.js
@@ -16,3 +16,13 @@ export const connectRabbitMQ = async () => {
         console.log("Failed to connect to rabbitmq :", error);
     }
 };
+export const publishToQueue = async (queueName, message) => {
+    if (!channel) {
+        console.log("RabbitMQ channel is not initialized");
+        return;
+    }
+    await channel.assertQueue(queueName, { durable: true });
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+        persistent: true,
+    });
+};
--- user/src/config/rabbitmq.ts
+++ user/src/config/rabbitmq.ts
@@ -11,10 +11,21 @@ export const connectRabbitMQ = async () => {
       username: process.env.Rabbitmq_Username,
       password: process.env.Rabbitmq_Password,
     });
-    channel= await connection.createChannel();
+    channel = await connection.createChannel();
     console.log("✅ connected to rabbitmq");
-    
   } catch (error) {
     console.log("Failed to connect to rabbitmq :", error);
   }
+};
+
+export const publishToQueue = async (queueName: string, message: any) => {
+  if (!channel) {
+    console.log("RabbitMQ channel is not initialized");
+    return;
+  }
+
+  await channel.assertQueue(queueName, { durable: true });
+  channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+    persistent: true,
+  });
 };
