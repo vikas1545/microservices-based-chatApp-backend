c2beddcb1bcca8435a0ccefe580e17a49655bb72
vikas1545 <vikaskumar.vk0612@gmail.com>
2025-08-16 19:25:47 +0530

    centrlized tryCatch

--- user/src/config/rabbitmq.ts
+++ user/src/config/rabbitmq.ts
@@ -1,10 +1,10 @@
-import ampq from "amqplib";
+import amql from "amqplib";
 
-let channel: ampq.Channel;
+let channel: amql.Channel;
 
 export const connectRabbitMQ = async () => {
   try {
-    const connection = await ampq.connect({
+    const connection = await amql.connect({
       protocol: "amqp",
       hostname: process.env.Rabbitmq_Host,
       port: 5672,
