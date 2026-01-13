3208cb3f38e5e6573eb0410955cbf3ae33b9dee4
vikas1545 <vikaskumar.vk0612@gmail.com>
2026-01-13 23:06:13 +0530

    socket work for typing effect done

---
+++ chat/src/config/socket.ts
@@ -0,0 +1,65 @@
+import { Server, Socket } from "socket.io";
+import http from "http";
+import express from "express";
+
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap: Record<string, string> = {};
+io.on("connection", (socket: Socket) => {
+  console.log("User connected", socket.id);
+
+  const userId = socket.handshake.query.userId as string | undefined;
+
+  if (userId && userId !== "undefined") {
+    userSocketMap[userId] = socket.id;
+    console.log(`User ID ${userId} mapped to socket ID ${socket.id}`);
+  }
+
+  io.emit("getOnlineUser", Object.keys(userSocketMap));
+  if (userId) {
+    socket.join(userId);
+  }
+  socket.on("typing", (data) => {
+    console.log(`User ${data.userId} is typing in chat ${data.chatId}`);
+    socket.to(data.chatId).emit("userTyping", { userId: data.userId, chatId: data.chatId });
+  });
+
+  socket.on("stopTyping", (data) => {
+    console.log(`User ${data.userId} stopped typing in chat ${data.chatId}`);
+    socket.to(data.chatId).emit("userStoppedTyping", { userId: data.userId, chatId: data.chatId });
+  });
+
+  socket.on("joinChat", (chatId) => {
+    socket.join(chatId);
+    console.log(`User joined chat room ${chatId}`);
+  });
+
+  socket.on("leaveChat", (chatId) => {
+    socket.leave(chatId);
+    console.log(`User left chat room ${chatId}`);
+  })  
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+    if (userId) {
+      delete userSocketMap[userId];
+      console.log(`User ID ${userId} removed from online users`);
+      io.emit("getOnlineUser", Object.keys(userSocketMap));
+    }
+  });
+
+  socket.on("connect_error", (err) => {
+    console.log(`socket connection error: ${err}`);
+  });
+});
+
+export { app, server, io };
--- chat/src/index.ts
+++ chat/src/index.ts
@@ -3,16 +3,16 @@ import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import chatRoutes from "./routes/chat.js";
 import cors from 'cors';
+import { app, server } from "./config/Socket.js";
 
 dotenv.config();
 connectDb();
-const app = express();
 const port = process.env.PORT;
 app.use(cors())
 
 app.use(express.json());
 app.use("/api/v1", chatRoutes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`chat server started on ${port}`);
 });
